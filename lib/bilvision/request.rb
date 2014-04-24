c5cb765ba45a756f0b4c8d87a2ad8eaf244364e5
Tobias Sandelius <tobias@sandeli.us>
2014-04-24 07:47:26 +0200

    Allow to fetch raw hash from request

--- lib/bilvision/request.rb
+++ lib/bilvision/request.rb
@@ -23,39 +23,54 @@ module Bilvision
 
     # Fetches the `grundfraga` information.
     #
+    # @param raw [boolean]
+    #   Should we return the raw hash.
+    #
     # @return [Hash]
     #   Request response as a hash.
-    def general
-      fetch(:grundfraga)[:grundfraga_response][:grundfraga_result]
+    def general(raw = false)
+      data = fetch(:grundfraga)
+      raw ? data : data[:grundfraga_response][:grundfraga_result]
     end
 
     # Fetches the `teknikfraga_v2` information.
     #
+    # @param raw [boolean]
+    #   Should we return the raw hash.
+    #
     # @return [Hash]
     #   Request response as a hash.
-    def technical
-      fetch(:teknikfraga_v2)[:teknikfraga_v2_response][:teknikfraga_v2_result]
+    def technical(raw = false)
+      data = fetch(:teknikfraga_v2)
+      raw ? data : data[:teknikfraga_v2_response][:teknikfraga_v2_result]
     end
 
     # Fetches the `teknikfraga_v2` information.
     #
+    # @param raw [boolean]
+    #   Should we return the raw hash.
+    #
     # @return [Hash]
     #   Request response as a hash.
-    def owners
+    def owners(raw = false)
       data = fetch(:foregaende_agarefraga)
+      return data if raw
       data = data[:foregaende_agarefraga_response][:foregaende_agarefraga_result]
       data[:tidigare_agare][:struc_agare]
     end
 
     # Runs all request and return the result as a namespaced hash.
     #
+    # @param raw [boolean]
+    #   Should we return the raw hash.
+    #
     # @return [Hash]
     #   Request response as a namespaced hash.
-    def all
+    def all(raw = false)
       {
-        general:   general,
-        technical: technical,
-        owners:    owners
+        general:   general(raw),
+        technical: technical(raw),
+        owners:    owners(raw)
       }
     end
 
